1c957530f5014f23098bba07a5622628372db2ff
Daniel Azil <danielazil.fse@gmail.com>
2024-07-04 00:27:08 +0000

    A module that solves the N queens problem

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,38 +1,46 @@
-
 #!/usr/bin/python3
-"""challenge of placing N non-attacking queens on an N×N chessboard"""
+"""
+    A module that solves the N queens problem
+"""
 import sys
 
 
-def nonAttack(brd, ln, i):
-    '''checks a place is not attacked by queens'''
-    for j in range(ln):
-        if(brd[j] == i or brd[j] + ln - j == i or brd[j] + j - ln == i):
+def check_attack(board, arg1, i):
+    """
+        A function that checks if a position is not
+        attacked by queens in the previous rows
+    """
+    for each_arg in range(arg1):
+        if (board[each_arg] == i or
+                board[each_arg] + arg1 - each_arg == i or
+                board[each_arg] + each_arg - arg1 == i):
             return False
     return True
 
 
-def fillBoard(brd, ln):
-    '''finds the next safe posn to land the queen'''
-    for i in range(len(brd)):
-        if nonAttack(brd, ln, i):
-            brd[ln] = i
-            if ln < len(brd) - 1:
-                fillBoard(brd, ln + 1)
+def safe_pos(board, arg1):
+    """
+        A function that checks for safe postions
+    """
+    for i in range(len(board)):
+        if check_attack(board, arg1, i):
+            board[arg1] = i
+            if arg1 < len(board) - 1:
+                safe_pos(board, arg1 + 1)
             else:
-                print([[i, brd[i]] for i in range(len(brd))])
+                print([[i, board[i]] for i in range(len(board))])
 
 
 if len(sys.argv) != 2:
     print("Usage: nqueens N")
     sys.exit(1)
 try:
-    n = int(sys.argv[1])
+    val = int(sys.argv[1])
 except Exception:
     print("N must be a number")
     sys.exit(1)
-if n < 4:
+if val < 4:
     print("N must be at least 4")
     sys.exit(1)
-brd = [-1 for i in range(n)]
-fillBoard(brd, 0)
+board = [-1 for i in range(val)]
+safe_pos(board, 0)
