74cd7b7c6c33d875a14ddae610f15a30ac652e98
Daniel-Azil <125911921+Daniel-Azil@users.noreply.github.com>
2024-08-14 16:01:56 +0000

    Update 0-rotate_2d_matrix.py

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,16 +1,16 @@
 """
-    A module that rotates a 2D matrix clockwise in 90 degrees.
+A module that rotates a 2D matrix clockwise by 90 degrees.
 """
-
 
 def rotate_2d_matrix(matrix):
     """
-        A function that rotates a 2D matrix.
+    A function that rotates a 2D matrix 90 degrees clockwise.
     """
-    each_row = len(matrix) - 1
+    n = len(matrix)
     each_ele = 0
+    each_row = n - 1
 
-    while each_row > each_ele:
+    while each_ele < each_row:
         for i in range(each_ele, each_row):
             value_tmp = matrix[i][each_row]
             matrix[i][each_row] = matrix[each_ele][i]
@@ -18,6 +18,6 @@ def rotate_2d_matrix(matrix):
             matrix[each_row][each_row + each_ele - i] = value_tmp
             value_tmp = matrix[each_row + each_ele - i][each_ele]
             matrix[each_row + each_ele - i][each_ele] = value_tmp2
-            matrix[each_ele][i] = tmp
+            matrix[each_ele][i] = value_tmp
         each_ele += 1
         each_row -= 1
